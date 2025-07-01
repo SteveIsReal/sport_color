9bfead4078c5e8ecdc86ab668d1bf43018be7ad7
Pyn Chaingin <stevie@Pyns-MacBook-Pro.local>
2025-07-01 08:46:36 +0700

    admin update

--- buildboard/models.py
+++ buildboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+COLORS = (('red','red'),('yellow','yellow'),('orange','orange'),('blue','blue'))
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=6, choices=COLORS)
+
+    def __str__(self):
+        return f'{self.name} : {self.color}'
 
