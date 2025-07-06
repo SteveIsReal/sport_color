5fc6fefc6c015aff23bf6bf90ef40905e7f23cb4
Pyn Chaingin <stevie@Pyns-MacBook-Pro.local>
2025-07-06 10:39:38 +0700

    tournament update

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Tournament, Score
 
-# Register your models here.
+
+@admin.register(Tournament)
+class AdminTournament(admin.ModelAdmin):
+    pass
+
+@admin.register(Score)
+class AdminScore(admin.ModelAdmin):
+    pass
--- tournament/models.py
+++ tournament/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from buildboard.models import Team, Game
 
-# Create your models here.
+
+class Tournament(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team1")
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team2")
+
+class Score(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    game_set = models.IntegerField(default=0)
+    score1 = models.IntegerField(default=0)
+    score2 = models.IntegerField(default=0)
+    
